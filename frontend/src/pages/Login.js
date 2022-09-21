06d52944366d5e3c07dc97d029725f43a5a0125b
Fayik <fayik.fcb@gmail.com>
2022-09-21 19:37:37 +0400

    Update

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -16,7 +16,7 @@ const Login = () => {
     return (
         <form className="login" onSubmit={handleSubmit}>
             <h3> Log In </h3>
-
+            
             <label> Email: </label>
             <input type="email" onChange={(e) => setEmail(e.target.value)} value={email} />
             <label> Password: </label>
